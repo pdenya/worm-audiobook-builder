c5786a462c7a770598ef17309a90f74b622a9b37
Paul Denya <pdenya@gmail.com>
2022-01-31 11:03:01 -0500

    audiobooks that are too big are failing, started splitting them up further

--- abb.sh
+++ abb.sh
@@ -1,12 +1,22 @@
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-1.txt \
--t "Worm - Book 1" \
--o "Worm - Book 1.m4b"
+-t "Worm - Book 1a" \
+-o "Worm - Book 1a.m4b"
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-1b.txt \
+-t "Worm - Book 1b" \
+-o "Worm - Book 1b.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-2.txt \
--t "Worm - Book 2" \
--o "Worm - Book 2.m4b"
+-t "Worm - Book 2a" \
+-o "Worm - Book 2a.m4b"
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-2b.txt \
+-t "Worm - Book 2b" \
+-o "Worm - Book 2b.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-3.txt \
