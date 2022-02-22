62831e9fba462011f51ade09780784a5333bc89f
Paul Denya <pdenya@gmail.com>
2022-02-22 15:51:43 -0500

    book split

--- abb.sh
+++ abb.sh
@@ -1,35 +1,50 @@
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-1.txt \
--t "Worm - Book 1a" \
--o "Worm - Book 1a.m4b"
+-t "Worm - Book 1" \
+-o "Worm - Book 1.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-1b.txt \
--t "Worm - Book 1b" \
--o "Worm - Book 1b.m4b"
+-t "Worm - Book 2" \
+-o "Worm - Book 2.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
 -i abb-input-book-2.txt \
--t "Worm - Book 2a" \
--o "Worm - Book 2a.m4b"
-
-./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
--i abb-input-book-2b.txt \
--t "Worm - Book 2b" \
--o "Worm - Book 2b.m4b"
-
-./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
--i abb-input-book-3.txt \
 -t "Worm - Book 3" \
 -o "Worm - Book 3.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
--i abb-input-book-4.txt \
+-i abb-input-book-2b.txt \
 -t "Worm - Book 4" \
 -o "Worm - Book 4.m4b"
 
 ./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
--i abb-input-book-5.txt \
+-i abb-input-book-3.txt \
 -t "Worm - Book 5" \
 -o "Worm - Book 5.m4b"
 
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-3b.txt \
+-t "Worm - Book 6" \
+-o "Worm - Book 6.m4b"
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-4.txt \
+-t "Worm - Book 7" \
+-o "Worm - Book 7.m4b"
+
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-4b.txt \
+-t "Worm - Book 8" \
+-o "Worm - Book 8.m4b"
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-5.txt \
+-t "Worm - Book 9" \
+-o "Worm - Book 9.m4b"
+
+./abbinder -v -E "%t %a" -a "J.C. McCrae (Wildbow)" -C ./cover.jpg -b 128 \
+-i abb-input-book-5b.txt \
+-t "Worm - Book 10" \
+-o "Worm - Book 10.m4b"
